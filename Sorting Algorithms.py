548e2d95df02807629040c37f310e8a209e09e49
Dayalan D S <dayalands2210@gmail.com>
2025-10-16 02:42:20 +0530

    Oct 16 updates

--- Sorting Algorithms.py
+++ Sorting Algorithms.py
@@ -1,6 +1,6 @@
 #Bubble Sort
 
-def bubble_sort(arr):
+def simple_bubble_sort(arr):
 
     n = len(arr)
     for i in range(n-1):
@@ -13,13 +13,47 @@ def bubble_sort(arr):
             return arr
     return arr
 
+def complex_bubble_sort(elements, key):
+    for i in range(len(elements)-1):
+        swapped = False
+        for j in range(len(elements)-1-i):
+            if elements[j][key] > elements[j+1][key]:
+                elements[j][key],elements[j + 1][key] = elements[j + 1][key],elements[j][key]
+                swapped = True
 
+        if not swapped:
+            return elements
+
+    return elements
 
 
 if __name__ == '__main__':
 
     # elements = [ 5, 81, 64, 1, 2, 39, 40, 10]
-    elements = [1, 2, 5, 10, 39, 40, 64, 81]
+    # elements = [1, 2, 5, 10, 39, 40, 64, 81]
     names = ["daya","devi","michael","andrew","jeni","rajan","joy","kiran"]
-    # print(bubble_sort(names))
+    print(simple_bubble_sort(names))
+
+    elements = [
+        {'name': 'mona', 'transaction_amount': 1000, 'device': 'iphone-10'},
+        {'name': 'dhaval', 'transaction_amount': 400, 'device': 'google pixel'},
+        {'name': 'kathy', 'transaction_amount': 200, 'device': 'vivo'},
+        {'name': 'aamir', 'transaction_amount': 800, 'device': 'iphone-8'},
+    ]
+    print(complex_bubble_sort(elements,key='transaction_amount'))
+
+    #output:
+    # [{'name': 'mona', 'transaction_amount': 200, 'device': 'iphone-10'},
+    #  {'name': 'dhaval', 'transaction_amount': 400, 'device': 'google pixel'},
+    #  {'name': 'kathy', 'transaction_amount': 800, 'device': 'vivo'},
+    #  {'name': 'aamir', 'transaction_amount': 1000, 'device': 'iphone-8'}]
+
+    print(complex_bubble_sort(elements, key='name'))
+
+    # output:
+    # [{'name': 'aamir', 'transaction_amount': 200, 'device': 'iphone-10'},
+    #  {'name': 'dhaval', 'transaction_amount': 400, 'device': 'google pixel'},
+    #  {'name': 'kathy', 'transaction_amount': 800, 'device': 'vivo'},
+    #  {'name': 'mona', 'transaction_amount': 1000, 'device': 'iphone-8'}]
+
 
